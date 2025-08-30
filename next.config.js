c2801e597b20e8228de159eed0f00f37510a13ea
masa162 <belong2jazz@gmail.com>
2025-08-30 09:25:45 +0900

    0830_03

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // ここに設定を追加できますが、今回は空のままでOKです。
 };
 
-export default nextConfig;
+module.exports = nextConfig;
